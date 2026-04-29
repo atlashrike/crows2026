a7f37cee20062c0767cddb1f8e38a5dc74ec03f4
Tabris Cao <169078296+atlashrike@users.noreply.github.com>
2026-04-29 13:35:48 -0400

    Add files via upload

---
+++ metadata/species_samples.py
@@ -0,0 +1,18 @@
+species_samples = {
+    "american_crow": [
+        "SRX542259",
+        "SRX542257",
+        "SRX542255",
+        "SRX542252",
+        "SRX542247",
+        "SRX535391",
+    ],
+    "rook": [
+        "SRX581055",
+        "SRX581052",
+    ],
+    "jackdaw": [
+        "SRX581043",
+        "SRX581042",
+    ]
+}
